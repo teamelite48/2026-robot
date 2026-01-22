cbe3566deeaa0d6be57b5d0a1cea84bbca9f67c3
Elite Coder <teamelitecoder@gmail.com>
2026-01-22 17:48:38 -0500

    Successful Build

--- src/main/java/frc/robot/subsystems/vision/VisionCommands.java
+++ src/main/java/frc/robot/subsystems/vision/VisionCommands.java
@@ -7,38 +7,38 @@ import frc.robot.RobotContainer;
 
 public class VisionCommands {
 
-  final static VisionSubsystem frontVisionSubsystem = RobotContainer.frontVisionSubsystem;
-  final static VisionSubsystem rearVisionSubsystem = RobotContainer.rearVisionSubsystem;
+  // final static VisionSubsystem frontVisionSubsystem = RobotContainer.frontVisionSubsystem;
+  // final static VisionSubsystem rearVisionSubsystem = RobotContainer.rearVisionSubsystem;
 
-  public static Command trackReef() {
-    return Commands.runOnce(() -> {
-      frontVisionSubsystem.stopTracking();
-      rearVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.ReefApriltag);
-      RobotContainer.visionTrackingMode = VisionTrackingMode.Rear;
-    });
-  }
+  // public static Command trackReef() {
+  //   return Commands.runOnce(() -> {
+  //     frontVisionSubsystem.stopTracking();
+  //     rearVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.ReefApriltag);
+  //     RobotContainer.visionTrackingMode = VisionTrackingMode.Rear;
+  //   });
+  // }
 
-  public static Command trackLoadStation() {
-    return Commands.runOnce(() -> {
-      rearVisionSubsystem.stopTracking();
-      frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.LoadStationAprilTag);
-      RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
-    });
-  }
+  // public static Command trackLoadStation() {
+  //   return Commands.runOnce(() -> {
+  //     rearVisionSubsystem.stopTracking();
+  //     frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.LoadStationAprilTag);
+  //     RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
+  //   });
+  // }
 
-  public static Command trackBarge() {
-    return Commands.runOnce(() -> {
-      rearVisionSubsystem.stopTracking();
-      frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.BargeAprilTag);
-      RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
-    });
-  }
+  // public static Command trackBarge() {
+  //   return Commands.runOnce(() -> {
+  //     rearVisionSubsystem.stopTracking();
+  //     frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.BargeAprilTag);
+  //     RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
+  //   });
+  // }
 
-  public static Command trackProcessor() {
-    return Commands.runOnce(() -> {
-      rearVisionSubsystem.stopTracking();
-      frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.ProcessorAprilTag);
-      RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
-    });
-  }
+  // public static Command trackProcessor() {
+  //   return Commands.runOnce(() -> {
+  //     rearVisionSubsystem.stopTracking();
+  //     frontVisionSubsystem.startTracking(VisionSubsystem.VisionTarget.ProcessorAprilTag);
+  //     RobotContainer.visionTrackingMode = VisionTrackingMode.Front;
+  //   });
+  // }
 }
