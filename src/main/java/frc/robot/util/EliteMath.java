8369c156a062831bc482125d7b4b80d386893aed
Elite Coder <teamelitecoder@gmail.com>
2026-02-24 19:27:29 -0500

    Add Missing Map Function

--- src/main/java/frc/robot/util/EliteMath.java
+++ src/main/java/frc/robot/util/EliteMath.java
@@ -9,4 +9,20 @@ public class EliteMath {
   public static double clamp(double n, double min, double max) {
     return Math.max(min, Math.min(max, n));
   }
+
+  public static double map(double value, double inMin, double inMax, double outMin, double outMax) {
+    if (value < inMin) value = inMin;
+    if (value > inMax) value = inMax;
+
+    double inRange = inMax - inMin;
+    if (inRange == 0) {
+        // Avoid division by zero - just return the outMin or outMax as a fallback
+        return (value <= inMin) ? outMin : outMax;
+    }
+
+    double outRange = outMax - outMin;
+    double scaled = (value - inMin) / inRange;
+
+    return outMin + (scaled * outRange);
+  }
 }
