0554665a4b4884d75416d4f8851d5a795e53c644
Elite Coder <teamelitecoder@gmail.com>
2026-03-23 19:20:41 -0400

    Add PID Another Constructor

--- src/main/java/frc/robot/lib/PIDParameters.java
+++ src/main/java/frc/robot/lib/PIDParameters.java
@@ -5,8 +5,16 @@ public class PIDParameters {
     public final double P;
     public final double I;
     public final double D;
-    public final double S;
-    public final double V;
+    public Double S = null;
+    public Double V = null;
+
+    public PIDParameters(double p, double i, double d) {
+        this.P = p;
+        this.I = i;
+        this.D = d;
+        this.S = 0.0;
+        this.V = 0.0;
+    }
 
     public PIDParameters(double p, double i, double d, double s, double v) {
         this.P = p;
