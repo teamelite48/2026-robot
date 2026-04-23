fd0a34f319574492bd228143872b106ad15c98f0
Trevor <trev111490@gmail.com>
2026-04-23 19:09:18 -0400

    increase intake speed

--- src/main/java/frc/robot/subsystems/intake/IntakeConfig.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConfig.java
@@ -9,7 +9,7 @@ import frc.robot.components.motors.lib.MotorConfig;
 
 public class IntakeConfig {
 
-    public static final double INTAKE_SPEED = -0.55;
+    public static final double INTAKE_SPEED = -0.65;
 
     public static MotorConfig getIntakeRightConfig() {
 
