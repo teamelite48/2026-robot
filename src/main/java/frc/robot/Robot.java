cfd088c4c966e6f98281999e75b56f33f1c3e805
Elite Coder <teamelitecoder@gmail.com>
2026-05-08 20:28:48 -0400

    Added AdvantageKit and simple loggers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,24 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LoggedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.FieldData;
 
-public class Robot extends TimedRobot {
+// Add imports for datalogging
+import org.littletonrobotics.junction.Logger;
+
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    // LoggedRobot starts/stops the datalogger for you.
+    // Optional: initialize AdvantageKit logger metadata
+    Logger.recordMetadata("ProjectName", "2026 Robot");
+
     m_robotContainer = new RobotContainer();
   }
 
